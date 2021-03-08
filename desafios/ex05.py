e81712d5e02c14c029032ebbb7faa4129a16b594
henrique <https://github.com/geviewski>
2021-03-08 11:14:00 -0300

    arrumando o programa

--- desafios/ex05.py
+++ desafios/ex05.py
@@ -1,2 +1,2 @@
 numero= int(input('digite um numero: '))
-print('o numero digitado foi {}, e seu sucessor é {} e seu antecessor {}'.format(numero, numero + 1, numero - 1))
+print('o numero digitado foi {}, e seu sucessor é {} e seu antecessor {}'.format(numero, (numero+1), (numero-1)))

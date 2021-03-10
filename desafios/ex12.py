9b701b61d4be40f06199f9688125f25ab027af7d
henrique <https://github.com/geviewski>
2021-03-10 16:09:18 -0300

    arrumando o programa

--- desafios/ex12.py
+++ desafios/ex12.py
@@ -1,2 +1,2 @@
 reais= float(input('Digite o valor do produto: '))
-print('O valor do produto é R$:{} e com 5% de desconto fica por R$:{}'.format(reais, reais - (reais * 5/100)))
+print('O valor do produto é R$:{} e com 5% de desconto fica por R$:{:.5}'.format(reais, reais - (reais * 5/100)))

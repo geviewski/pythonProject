8ee21bd0461dbf07c1c8438c74abcddfb6e65865
henrique <https://github.com/geviewski>
2021-03-12 17:35:45 -0300

    ex novo

---
+++ desafios/ex17.py
@@ -0,0 +1,4 @@
+cateto_adj = float(input('Digite o valor do cateto adjacente: '))
+cateto_opo = float(input('Digite o valor do cateto oposto:'))
+hipotenusa = (cateto_adj ** 2 + cateto_opo ** 2) ** (1/2)
+print(hipotenusa)

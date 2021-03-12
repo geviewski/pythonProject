b8a11bfb517f219d51dcfb24c66406313ef7a3a5
henrique <https://github.com/geviewski>
2021-03-12 20:44:06 -0300

    (ex novo)

--- desafios/ex19.py
+++ desafios/ex19.py
@@ -2,4 +2,4 @@ from random import choices
 print('vamos escolher um nome para apagar o quadro: ')
 alunos = ('henrique', 'gabriel', 'fernando', 'kelly')
 escolha = choices(alunos)
-print(escolha)
+print('O aluno selecionado foi: {} '.format(escolha))

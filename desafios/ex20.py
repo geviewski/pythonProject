b3ad77a89e4d334ad56691668b5e9bf784d5a885
henrique <https://github.com/geviewski>
2021-03-15 15:27:21 -0300

    (ex novo)

---
+++ desafios/ex20.py
@@ -0,0 +1,9 @@
+from random import shuffle
+nome1 = input('Digite o nome do primeiro aluno: ')
+nome2 = input('Digite o nome do segundo aluno: ')
+nome3 = input('Digite o nome do terceiro aluno: ')
+nome4 = input('Digite o nome do quarto aluno: ')
+lista = [nome1, nome2, nome3, nome4]
+shuffle(lista)
+print('Ordem de apresentação: ')
+print(lista)

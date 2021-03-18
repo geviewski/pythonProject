84b1f20351b3111e1daa1eb1cd7118ad21791fd0
henrique <https://github.com/geviewski>
2021-03-18 14:20:53 -0300

    arrumando o ex

---
+++ aulas/aula10(2).py
@@ -0,0 +1,8 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a primeira nota: '))
+media = (nota1 + nota2) / 2
+print('Sua media é {}'.format(media))
+if media >= 6:
+    print('voce esta com a media boa!')
+else:
+    print('voce tem que melhorar!')

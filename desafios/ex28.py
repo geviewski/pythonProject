8089317c4bd7db0a62eb21a9e8ec1eb8118f29d7
henrique <https://github.com/geviewski>
2021-03-18 18:21:01 -0300

    arrumando o ex

---
+++ desafios/ex28.py
@@ -0,0 +1,9 @@
+from random import randrange
+aleatorio = randrange(0, 6)
+numero = int(input('Digite um numero entre 0 a 5, vejamos se é o mesmo que o do computador: '))
+print('Analizando...')
+if numero == aleatorio:
+    print('Parabens voce acertou!')
+else:
+    print('Voce não acertou!')
+print('o numero que o computador pensou foi o {} e o seu numero é {}'.format(aleatorio, numero))

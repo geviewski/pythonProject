87caf0b22fe717cae60e8452bf6b158bbc1d464d
henrique <https://github.com/geviewski>
2021-03-24 13:27:06 -0300

    arrumando o ex

--- desafios/ex38.py
+++ desafios/ex38.py
@@ -1,1 +1,12 @@
-num1 = int(input(''))
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+maior = num2
+if num1 > num2:
+    maior = num1
+    print('O maior numero é o {}'.format(maior))
+maior = num1
+if num2 > num1:
+    maior = num2
+    print('O maior numero é o {}'.format(maior))
+if num1 == num2:
+    print('os valores são iguais.')

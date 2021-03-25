238b06b65fc9d5dfc9594e6f8b386be73f4225b7
henrique <https://github.com/geviewski>
2021-03-25 18:08:32 -0300

    arrumando o ex

--- desafios/ex42.py
+++ desafios/ex42.py
@@ -2,8 +2,12 @@ lado1 = int(input('Digite o Primeiro numero: '))
 lado2 = int(input('Digite o Segundo numero: '))
 lado3 = int(input('Digite o terceiro numero: '))
 if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2:
-    print('Os numeros podem formar um triangulo!')
-    if lado1 == lado3 + lado2 and lado2 == lado3 + lado1 and lado3 == lado2 + lado1:
-        print('é um triangulo equilatero!')
+    print('Os numeros podem formar um triangulo ', end='')
+    if lado1 == lado2 == lado3:
+        print('EQUILATERO!!')
+    elif lado1 != lado2 != lado3 != lado1:
+        print('ESCALENO!!')
+    else:
+        print('ISÓSCELES!')
 else:
     print('Os numeros nao podem formar um triangulo')

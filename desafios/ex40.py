c1801b2ce7536c1d1eaf30b4a399a950a302e78e
henrique <https://github.com/geviewski>
2021-03-25 17:52:39 -0300

    arrumando o ex

--- desafios/ex40.py
+++ desafios/ex40.py
@@ -3,7 +3,7 @@ nota2 = float(input('Digite a sua segunda nota: '))
 media = (nota1 + nota2) / 2
 if media < 5:
     print('Aluno reprovado! Sua media é {}'.format(media))
-if media>=5 and media<=6.9 :
+if media >=5  and media < 7 :
     print('Aluno em recuperação! Sua media é {}'.format(media))
 if media >= 7:
     print('Aluno aprovado! Sua media é {}'.format(media))

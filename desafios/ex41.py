505b5343ca2e4be4b748a88a29438f72cb916021
henrique <https://github.com/geviewski>
2021-03-25 17:57:03 -0300

    arrumando o ex

--- desafios/ex41.py
+++ desafios/ex41.py
@@ -4,11 +4,11 @@ data_nasc = int(input('Digite a data de nascimento do competidor para ver em qua
 data = ano_atual - data_nasc
 if data <= 9:
     print('Voce tem {} anos e está na categoria: Mirin!'.format(data))
-if data > 9 and data <= 14:
+elif data > 9 and data <= 14:
     print('Voce tem {} anos e está na categoria: Infantil!'.format(data))
-if data > 14 and data <= 19:
+elif data > 14 and data <= 19:
     print('Voce tem {} anos e está na categoria: Junior!'.format(data))
-if data > 19 and data <=20:
+elif data > 19 and data <=25:
     print('Voce tem {} anos e está a categoria: Sênior!'.format(data))
-elif data > 20:
+else:
     print('Voce tem {} anos e está na categoria: Master!'.format(data))

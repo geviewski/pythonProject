cbabdb5be9e68b3add73a7926ffba444d48a6623
henrique <https://github.com/geviewski>
2021-03-29 19:54:20 -0300

    arrumando o ex

--- desafios/ex52.py
+++ desafios/ex52.py
@@ -1,5 +1,14 @@
 num = int(input('Digite um numero: '))
-if num / 1 == 0 or num / num == 0:
-    print('primo')
+total = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end='')
+        total += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divisivel {} vezes'.format(num, total), end='')
+if total == 2:
+    print(' E por isso ele é um número PRIMO!')
 else:
-    print('nao é primo')
+    print(' E por isso ele NÃO é um número PRIMO!')

6a1f2092bb2bb4c07984f22d288d28305348f297
henrique <https://github.com/geviewski>
2021-03-29 17:32:26 -0300

    arrumando o ex

--- desafios/ex48.py
+++ desafios/ex48.py
@@ -1,5 +1,5 @@
-s = 0
-for c in range(1, 500, 2):
+soma = 0
+for c in range(1, 501, 2):
     if c % 3 == 0:
-        s+=c
-    print(s)
+        soma += c
+print('A soma de todos os valores é {}'.format(soma))

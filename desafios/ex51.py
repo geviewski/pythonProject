fcb9e8a0a3debb754e98ffd44a41aaa8c8729293
henrique <https://github.com/geviewski>
2021-03-29 19:35:18 -0300

    arrumando o ex

--- desafios/ex51.py
+++ desafios/ex51.py
@@ -1,7 +1,5 @@
 primeiro = int(input('Primeiro elemento: '))
 razao = int(input('Razão: '))
-n = int(input('Quantos elementos exibir: '))
-ultimo = primeiro + (n - 1) * razao
-ultimo += ultimo
-for var in range(primeiro, ultimo, razao):
-    print(var)
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print(c)

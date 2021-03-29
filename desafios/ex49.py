97e8f862686f9d9397528197c9edb3a7b7b9f13d
henrique <https://github.com/geviewski>
2021-03-29 17:38:14 -0300

    arrumando o ex

--- desafios/ex49.py
+++ desafios/ex49.py
@@ -1,4 +1,4 @@
-num1 = int(input('\033[0;35;40m Digite um numero: \033[m '))
+num = int(input('\033[0;35;40m Digite um numero: \033[m '))
 for c in range(0, 11):
-    print('\033[0;35;40m {} x {} = {} \033[m'.format(num1, c, num1*c))
+    print('\033[0;35;40m {} x {} = {} \033[m'.format(num, c, num*c))
 print('\033[0;35;40m Fim! \033[m')

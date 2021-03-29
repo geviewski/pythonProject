fab63922f62933c273271c1c46e7cdc26f311add
henrique <https://github.com/geviewski>
2021-03-29 17:19:58 -0300

    arrumando o ex

--- desafios/ex46.py
+++ desafios/ex46.py
@@ -1,5 +1,5 @@
 from time import sleep
-for c in range(11, 0, -1):
+for c in range(10, -1, -1):
     sleep(1)
     print('{}..'.format(c))
 print('fim!')
---
+++ desafios/ex52.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um numero: '))
+if num / 1 == 0 or num / num == 0:
+    print('primo')
+else:
+    print('nao é primo')

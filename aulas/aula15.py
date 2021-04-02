fae0ca846ecd0cc31030d13a41d8eae24aab2250
henrique <https://github.com/geviewski>
2021-04-02 15:36:28 -0300

    maior menor while

---
+++ aulas/aula15.py
@@ -0,0 +1,10 @@
+'''n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')'''
+num1 = 3
+num2 = 2
+print(f'os valores digitados foram {num1} e {num2} e a soma deles é {num1+num2}')
